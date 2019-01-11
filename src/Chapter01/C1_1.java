268f178e5b4dccf8ca7a6f4c645792d37bc62487
ChrisTjahjo <>
2019-01-11 12:58:57 -0600

    Chapter 01 - Chapter 04

---
+++ src/Chapter01/C1_1.java
@@ -0,0 +1,22 @@
+package Chapter01;
+
+/**
+ * Program says welcome to java
+ *
+ * @author Chris Tjahjo
+ */
+public class C1_1 {
+
+    /**
+     * Main method
+     *
+     * @param args command line argument
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+    }
+}
---
+++ src/Chapter01/C1_2.java
@@ -0,0 +1,18 @@
+package Chapter01;
+
+/**
+ * Program adds numbers
+ *
+ * @author Chris Tjahjo
+ */
+public class C1_2 {
+
+    /**
+     * Main method
+     *
+     * @param args command line argument
+     */
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
